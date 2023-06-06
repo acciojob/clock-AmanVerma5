58a30facab2ed023830ed4a93d085cd6395eb339
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-06 14:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let timer=document.getElementById("timer");
+
+function changeTime(){
+	let date=new Date();
+	timer.innerText=date;
+}
+setInterval(changeTime,1000);
